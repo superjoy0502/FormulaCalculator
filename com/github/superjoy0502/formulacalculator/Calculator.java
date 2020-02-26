f71494136a690398f7590a9741e64399a29ac4f9
superjoy0502 <superjoy0502@gmail.com>
2020-02-25 20:42:21 -0700

    add license

--- com/github/superjoy0502/formulacalculator/Calculator.java
+++ com/github/superjoy0502/formulacalculator/Calculator.java
@@ -1,3 +1,23 @@
+
+// Copyright (C) 2020  superjoy0502
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
 package com.github.superjoy0502.formulacalculator;
 
 import javax.swing.JOptionPane;
